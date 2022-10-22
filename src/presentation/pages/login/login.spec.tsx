a68f96b3b1276d46a1f121204c2c5e0bdf842889
Breno <bhfs.2908@gmail.com>
2022-10-22 20:18:58 -0300

    test: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -48,12 +48,21 @@ describe("Login Component", () => {
     expect(passwordStatus.textContent).toBe("🔴");
   });
 
-  test("Should call Validation with correct values", () => {
+  test("Should call Validation with correct email", () => {
     const { validationSpy } = makeSut();
     const emailInput = screen.getByRole("email");
     fireEvent.input(emailInput, { target: { value: "any_email" } });
     expect(validationSpy.input).toEqual({
       email: "any_email",
+    });
+  });
+
+  test("Should call Validation with correct password", () => {
+    const { validationSpy } = makeSut();
+    const passwordInput = screen.getByRole("password");
+    fireEvent.input(passwordInput, { target: { value: "any_password" } });
+    expect(validationSpy.input).toEqual({
+      password: "any_password",
     });
   });
 });
