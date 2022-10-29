e5cb41f8c1fa148000e03fbe0de6a2a9ed88a231
Breno <bhfs.2908@gmail.com>
2022-10-29 16:12:05 -0300

    feat: ensure MinLengthValidation returns falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.ts
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -5,6 +5,8 @@ export class MinLengthValidation implements FieldValidation {
   constructor(readonly field: string, private readonly minLength: number) {}
 
   validate(value: string): any {
-    return new InvalidFieldError();
+    return value.trim().length >= this.minLength
+      ? null
+      : new InvalidFieldError();
   }
 }
