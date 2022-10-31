b9c6417a5cfd4acf4af143ef570e68379410f76b
Breno <bhfs.2908@gmail.com>
2022-10-31 08:48:31 -0300

    test: ensure ValidationComposite uses correct validations

---
+++ src/main/factories/pages/login/login-validation-factory.spec.ts
@@ -0,0 +1,15 @@
+import { ValidationComposite } from "@/validation/validators";
+import { ValidationBuilder as Builder } from "@/validation/validators/builder/validation-builder";
+import { makeLoginValidation } from "./login-validation-factory";
+
+describe("LoginValidationFactory", () => {
+  test("Should compose ValidationComposite with correct validations", () => {
+    const composite = makeLoginValidation();
+    expect(composite).toEqual(
+      ValidationComposite.build([
+        ...Builder.field("email").required().email().build(),
+        ...Builder.field("password").required().min(5).build(),
+      ])
+    );
+  });
+});
