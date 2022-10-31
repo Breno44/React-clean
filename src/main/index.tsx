505e2b3b2868adcdc95eebfc04d02fa627135530
Breno <bhfs.2908@gmail.com>
2022-10-31 07:45:12 -0300

    feat: add Login composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Router from "@/presentation/components/router/router";
 import "@/presentation/styles/global.scss";
+import { makeLogin } from "./factories/pages/login/login-factory";
 
-ReactDOM.render(<Router />, document.getElementById("main"));
+ReactDOM.render(
+  <Router MakeLogin={makeLogin} />,
+  document.getElementById("main")
+);
