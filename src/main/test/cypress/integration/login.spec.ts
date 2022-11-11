2274b242e9be286e0108e8870f108cee684bf6b2
Breno <breno@loldesign.com.br>
2022-11-11 19:52:24 -0300

    test: ensure login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,16 @@
-describe('Login', () => {
-  it('Should load with correct initial state', () => {
-    cy.visit('/login');
+describe("Login", () => {
+  beforeEach(() => {
+    cy.visit("/login");
+  });
+
+  it("Should load with correct initial state", () => {
+    cy.getByRole("email-status")
+      .should("have.attr", "title", "Campo obrigatório")
+      .should("contain.text", "🔴");
+    cy.getByRole("password-status")
+      .should("have.attr", "title", "Campo obrigatório")
+      .should("contain.text", "🔴");
+    cy.getByRole("submit").should("have.attr", "disabled");
+    cy.getByRole("error-wrap").should("not.have.descendants");
   });
 });
