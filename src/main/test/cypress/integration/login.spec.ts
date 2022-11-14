189080deebb798c6f531542782e02cc6eee3da32
Breno <breno@loldesign.com.br>
2022-11-14 16:53:37 -0300

    test: ensure Login present valid state if form is valid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -23,11 +23,24 @@ describe("Login", () => {
     cy.getByRole("email-status")
       .should("have.attr", "title", "Valor inválido")
       .should("contain.text", "🔴");
-    cy.getByRole("password").focus().type(faker.random.word());
+    cy.getByRole("password").focus().type(faker.random.alphaNumeric(3));
     cy.getByRole("password-status")
       .should("have.attr", "title", "Valor inválido")
       .should("contain.text", "🔴");
     cy.getByRole("submit").should("have.attr", "disabled");
+    cy.getByRole("error-wrap").should("not.have.descendants");
+  });
+
+  it("Should present valid state if form is valid", () => {
+    cy.getByRole("email").focus().type(faker.internet.email());
+    cy.getByRole("email-status")
+      .should("have.attr", "title", "Tudo certo!")
+      .should("contain.text", "🟢");
+    cy.getByRole("password").focus().type(faker.random.alphaNumeric(5));
+    cy.getByRole("password-status")
+      .should("have.attr", "title", "Tudo certo!")
+      .should("contain.text", "🟢");
+    cy.getByRole("submit").should("not.have.attr", "disabled");
     cy.getByRole("error-wrap").should("not.have.descendants");
   });
 });
