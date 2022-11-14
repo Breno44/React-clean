5d9b63203ce8ff0ac6b79e4db049ba09bd6782fb
Breno <breno@loldesign.com.br>
2022-11-14 16:50:16 -0300

    test: ensure Login present error state if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,14 +1,31 @@
+import faker from "faker";
+
 describe("Login", () => {
   beforeEach(() => {
     cy.visit("/login");
   });
 
   it("Should load with correct initial state", () => {
+    cy.getByRole("email").should("have.attr", "readOnly");
     cy.getByRole("email-status")
       .should("have.attr", "title", "Campo obrigatório")
       .should("contain.text", "🔴");
+    cy.getByRole("password").should("have.attr", "readOnly");
     cy.getByRole("password-status")
       .should("have.attr", "title", "Campo obrigatório")
+      .should("contain.text", "🔴");
+    cy.getByRole("submit").should("have.attr", "disabled");
+    cy.getByRole("error-wrap").should("not.have.descendants");
+  });
+
+  it("Should present error state if form is invalid", () => {
+    cy.getByRole("email").focus().type(faker.random.word());
+    cy.getByRole("email-status")
+      .should("have.attr", "title", "Valor inválido")
+      .should("contain.text", "🔴");
+    cy.getByRole("password").focus().type(faker.random.word());
+    cy.getByRole("password-status")
+      .should("have.attr", "title", "Valor inválido")
       .should("contain.text", "🔴");
     cy.getByRole("submit").should("have.attr", "disabled");
     cy.getByRole("error-wrap").should("not.have.descendants");
